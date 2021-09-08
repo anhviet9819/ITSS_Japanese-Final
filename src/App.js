abb4684412848635583aded85e4ddb6588504343
unknown <anhviet9819@gmail.com>
2021-09-08 11:25:18 +0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import CustomHook from './hooks/ListHook'
 
 function App() {
     const array = ['Huyen', 'Hoa', 'Hung', 'Long']
-    const [name, changeNewName, addName, arrayList] = CustomHook(array)
+    const [name, changeName, addName, arrayList] = CustomHook(array)
   
     return (
         <div className="App">
             <p>学生一覧：[ {array[0]}, {array[1]}, {array[2]}, {array[3]} ]</p>
             <p>追加する名前を入力してください。</p>
-            <input value={name} onChange={changeNewName} />
+            <input value={name} onChange={changeName} />
             <button onClick={addName} >確定</button>
             <p>追加する名前： {name}</p>
         </div>
